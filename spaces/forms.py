70276b469ebc1fddc00d578d7bcb4000c7513f75
Deoth <gueideoth@gmail.com>
2020-07-22 15:39:02 +0000

    modification de addhouseform

--- spaces/forms.py
+++ spaces/forms.py
@@ -30,17 +30,25 @@ class SignInForm(SignUpForm):
     password_verification = None
 
 
-class EditForm(SignUpForm):
-    client = None
-    landlord = None
+class EditForm(forms.Form):
+    username = forms.CharField(label='Pseudo : ', max_length=200, required=False , widget=forms.TextInput(attrs={
+        'class': 'inbox', 'title': 'Pseudo', 'placeholder': 'Entrez votre pseudo'}))
+    first_name = forms.CharField(label='Prenom : ', max_length=200, required=False ,widget=forms.TextInput(attrs={
+        'class': 'inbox', 'title': 'Prenom', 'placeholder': 'Entrez votre Prenom'}))
+    last_name = forms.CharField(label='Nom', max_length=200, required=False ,widget=forms.TextInput(attrs={
+        'class': 'inbox', 'title': 'Nom', 'placeholder': 'Entrez votre Nom'}))
+    email = forms.EmailField(label='Email : ', required=False, widget=forms.EmailInput(attrs={'class': 'inbox', 'title': 'Email', 'placeholder': 'Entrez votre addresse electronique', 'id': 'lo_em'}))
+    contact = forms.CharField(label='Contact : ',required=False , widget=forms.TextInput(attrs={'class': 'inbox', 'title': 'Contact', 'placeholder': 'Exemple : + 225 00-00-00-00', 'id': 'lo_nu'}))
+    password = forms.CharField(label='Mot de passe :',required=False ,  max_length=200, widget=forms.PasswordInput(attrs={'class': 'inbox', 'title': 'Mot de passe', 'placeholder': 'Entrez votre mot de passe'}))
+    password_verification = forms.CharField(label='Vérification', required=False, max_length=200, widget=forms.PasswordInput(attrs={'class': 'inbox', 'title': 'Vérification', 'placeholder': 'Entrez votre mot de passe à nouveau'}))
 
 
 class AddHouseForm(forms.Form):
     house_area = forms.CharField(max_length=50)
-    house_rent = forms.BigIntegerField()
-    house_deposit = forms.BigIntegerField()
+    house_rent = forms.IntegerField()
+    house_deposit = forms.IntegerField()
     house_kind = forms.CharField(max_length=50)
-    house_rooms_number = forms.PositiveSmallIntegerField()
+    house_rooms_number = forms.IntegerField()
     house_available = forms.BooleanField()
     house_to_sell = forms.BooleanField()
     house_image = forms.ImageField(upload_to="houses/")
