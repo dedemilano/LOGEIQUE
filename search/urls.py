236db7c23a16c2bcdbd63f8a6d1b6995b1709959
Deoth <gueideoth@gmail.com>
2020-08-12 15:59:14 +0000

    changement du chemin de search

--- search/urls.py
+++ search/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'search'
 
 urlpatterns = [
-    path('client', views.search_client, name='search_client'),
-    path('house', views.search_house, name='search_house'),
+    path('', views.search, name='search')
 ]
