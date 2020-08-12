735158fdf371b1c6fc2a66f753ddb4c4717633d6
Deoth <gueideoth@gmail.com>
2020-08-12 16:00:02 +0000

    changement de la classe de query

--- search/forms.py
+++ search/forms.py
@@ -1,8 +1,8 @@
 from django import forms 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label='', max_length= 200, widget=forms.TextInput(attrs={'id':'query', 'class':'search-bar', 'title':'Rechercher', 'placeholder':'exemple : Marcory...'}))
+    query = forms.CharField(label='', max_length= 200, widget=forms.TextInput(attrs={'id':'query', 'class':'form-control mr-sm-2', 'title':'Rechercher', 'placeholder':'exemple : Marcory...' , 'aria-label':"Search"}))
     query_for_all= forms.BooleanField(label='Tout', required=False, widget=forms.CheckboxInput(attrs={'id':'tout', 'checked':'checked'}))
-    query_for_house = forms.BooleanField(label='Canal+', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'hou'}))
-    query_for_client = forms.BooleanField(label='Startimes', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'cli'}))
-    query_for_lanlord = forms.BooleanField(label='Tnt', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'lan'}))
+    query_for_house = forms.BooleanField(label='Maison', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'hou'}))
+    query_for_client = forms.BooleanField(label='Client', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'cli'}))
+    query_for_lanlord = forms.BooleanField(label='Propietaire', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'lan'}))
