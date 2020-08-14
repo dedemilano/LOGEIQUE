c8d28fd48548391733de666d7c4f501c4dd56386
Deoth <gueideoth@gmail.com>
2020-08-14 15:31:56 +0000

    stylisation du formulaire searchform

--- search/forms.py
+++ search/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(label='', max_length= 200, widget=forms.TextInput(attrs={'id':'query', 'class':'form-control mr-sm-2', 'title':'Rechercher', 'placeholder':'exemple : Marcory...' , 'aria-label':"Search"}))
-    query_for_all= forms.BooleanField(label='Tout', required=False, widget=forms.CheckboxInput(attrs={'id':'tout', 'checked':'checked'}))
-    query_for_house = forms.BooleanField(label='Maison', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'hou'}))
-    query_for_client = forms.BooleanField(label='Client', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'cli'}))
-    query_for_lanlord = forms.BooleanField(label='Propietaire', required=False, widget=forms.CheckboxInput(attrs={'class':'stick', 'onclick':'active()', 'id':'lan'}))
+    query_for_all= forms.BooleanField(label='Tout', required=False, widget=forms.CheckboxInput(attrs={'id':'tout','class':'form-check-input stick' ,'checked':'checked'}))
+    query_for_house = forms.BooleanField(label='Maison', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check-input stick', 'onclick':'active()', 'id':'hou'}))
+    query_for_client = forms.BooleanField(label='Client', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check-input stick', 'onclick':'active()', 'id':'cli'}))
+    query_for_lanlord = forms.BooleanField(label='Propietaire', required=False, widget=forms.CheckboxInput(attrs={'class':'form-check-input stick', 'onclick':'active()', 'id':'lan'}))
